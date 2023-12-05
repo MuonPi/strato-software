e4f4f064009e1f624c652e49782770dc7058bc65
sm2671 <nico.krug@physik.uni-giessen.de>
2023-12-05 11:30:06 +0100

    added new library for sen0321

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,34 @@
 #include "i2cdevice.h"
-// #include "i2cdevice.cpp"
+#include "sen0321.h"
 // #include "ads1115.h"
 // #include "bme280.h"
 #include "qmc5883.h"
 // #include "veml6075.h"
 
-uint16_t x_raw, y_raw, z_raw, temperature_raw;
+// uint16_t x_raw, y_raw, z_raw, temperature_raw;
+// double x_real, y_real, z_real, temperature_real;
 
-double x_real, y_real, z_real, temperature_real;
+uint16_t ozone_raw;
 
 int main()
 {
-    QMC5883 Sensor_QMC5883;
-    Sensor_QMC5883.init();
-    Sensor_QMC5883.readRDYBit();
-    Sensor_QMC5883.getXYZRawValues(x_raw, y_raw, z_raw);
-    std::cout << "XYZ raw: " << x_raw << " " << y_raw << " "  << z_raw << std::endl;
-    Sensor_QMC5883.getXYZMagneticFields(x_real, y_real, z_real);
-    std::cout << "XYZ real: " << x_real << " " << y_real << " "  << z_real << std::endl;
-    Sensor_QMC5883.getTemperatureRawValue(temperature_raw);
-    std::cout << "Temperature raw: " << temperature_raw << std::endl;
-    Sensor_QMC5883.getTemperature(temperature_real);
-    std::cout << "Temperature real: " << temperature_real << std::endl;
+    // QMC5883 Sensor_QMC5883;
+    // Sensor_QMC5883.init();
+    // Sensor_QMC5883.readRDYBit();
+    // Sensor_QMC5883.getXYZRawValues(x_raw, y_raw, z_raw);
+    // std::cout << "XYZ raw: " << x_raw << " " << y_raw << " "  << z_raw << std::endl;
+    // Sensor_QMC5883.getXYZMagneticFields(x_real, y_real, z_real);
+    // std::cout << "XYZ real: " << x_real << " " << y_real << " "  << z_real << std::endl;
+    // Sensor_QMC5883.getTemperatureRawValue(temperature_raw);
+    // std::cout << "Temperature raw: " << temperature_raw << std::endl;
+    // Sensor_QMC5883.getTemperature(temperature_real);
+    // std::cout << "Temperature real: " << temperature_real << std::endl;
+
+
+
+
+    SEN0321 Sensor_SEN0321;
+    Sensor_SEN0321.init();
+    Sensor_SEN0321.getRawValue(ozone_raw);
+    std::cout << "Ozone raw: " << ozone_raw << std::endl;
 }
