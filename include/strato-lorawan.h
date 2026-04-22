265c695ae2721832001a658c57e996c85a674a79
sm2671 <sm2671@uni-giessen.de>
2026-04-22 16:51:06 +0200

    splitted lorawan in seperate files for easy use

--- include/strato-lorawan.h
+++ include/strato-lorawan.h
@@ -3,8 +3,9 @@
 #define _STRATO_LORAWAN_H_
 
 
-#include "hal/hal.h"
-#include "lmic.h"
+// #include "hal/hal.h"
+// #include "lmic.h"
+#include "lmic_lorawan.h"
 #include "globals.h"
 
 
@@ -23,13 +24,11 @@ public:
 
     bool init();
     bool sendPayload(uint8_t* payload, uint8_t size);
-    bool runloop();
+    void runloop();
     bool reset();
 
     bool setup(devaddr_t devaddr, const lmic_pinmap& lmic_pins, unsigned char *appskey, unsigned char *nwkskey);
-    void scheduleSendPayload(u1_t port, u4_t sequenceNo, uint8_t* message, uint8_t n);
     bool SeqNoFile(uint32_t& seqno);
-    // uint8_t PayloadFile(uint8_t* payload);
 
 private:
     Globals& StratoGlobals;
@@ -37,17 +36,6 @@ private:
     std::thread lorawanThread;
 };
 
-
-static uint8_t *data;
-static uint8_t data_size;
-extern bool txcomplete;
-
-void printEvent(ev_t ev);
-void onEvent(void *pUserData, ev_t ev);
-void do_send(osjob_t* j);
-
-void dump_rfm96_registers();
-uint8_t rfm96_read_register(int spi_fd, uint8_t reg);
 
 
 #endif // _STRATO_LORAWAN_H_
