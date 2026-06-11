b7adb36b2a8d71385bc97c2c67ac121183c48698
marvin5300 <37504638+marvin5300@users.noreply.github.com>
2026-06-11 13:53:30 +0200

    Added mqtt handler class

---
+++ src/strato-mqtthandler.cpp
@@ -0,0 +1,370 @@
+#include "mqtthandler.h"
+
+#include <QDebug>
+#include <QTimer>
+#include <QThread>
+#ifdef Q_OS_UNIX
+#include <unistd.h>
+#include <sys/syscall.h>
+#endif
+#include <cryptopp/sha.h>
+#include <cryptopp/filters.h>
+#include <cryptopp/hex.h>
+#include <string>
+#include <algorithm>
+
+#include <iostream>
+
+void wrapper_callback_connected(mosquitto * /*mqtt*/, void *object, int result)
+{
+    reinterpret_cast<StratoMqttHandler *>(object)->callback_connected(result);
+}
+
+void wrapper_callback_disconnected(mosquitto * /*mqtt*/, void *object, int result)
+{
+    reinterpret_cast<StratoMqttHandler *>(object)->callback_disconnected(result);
+}
+
+void wrapper_callback_message(mosquitto * /*mqtt*/, void *object, const mosquitto_message *message)
+{
+    reinterpret_cast<StratoMqttHandler *>(object)->callback_message(message);
+}
+
+void StratoMqttHandler::callback_connected(int result)
+{
+    if (result == 1)
+    {
+        qWarning() << "MQTT connection failed: Wrong protocol version";
+        emit connection_status(Status::Error);
+    }
+    else if (result == 2)
+    {
+        qWarning() << "MQTT connection failed: Credentials rejected";
+        emit connection_status(Status::Error);
+    }
+    else if (result == 3)
+    {
+        qWarning() << "MQTT connection failed: Broker unavailable";
+        emit connection_status(Status::Error);
+    }
+    else if (result > 3)
+    {
+        qWarning() << "MQTT connection failed: Other reason";
+        emit connection_status(Status::Error);
+    }
+    else if (result == 0)
+    {
+        qInfo() << "Connected to MQTT.";
+        emit connection_status(Status::Connected);
+        m_tries = 0;
+        return;
+    }
+    if (m_tries < s_max_tries)
+    {
+        m_tries++;
+    }
+    emit connection_status(Status::Connecting);
+    qDebug() << "Tried to connect " << m_tries << " times. Next try after " << (first_retry_period << m_tries) << "s.";
+}
+
+void StratoMqttHandler::callback_disconnected(int result)
+{
+    if (result != 0)
+    {
+        if (connected())
+        {
+            qWarning() << "MQTT disconnected unexpectedly:" + QString::number(result);
+            emit connection_status(Status::Error);
+        }
+    }
+    else
+    {
+        emit connection_status(Status::Disconnected);
+    }
+}
+
+void StratoMqttHandler::callback_message(const mosquitto_message *message)
+{
+    emit receivedMessage(message->topic, reinterpret_cast<char *>(message->payload));
+}
+
+void StratoMqttHandler::set_status(Status status)
+{
+    qDebug() << "set_status" << static_cast<int>(status);
+    m_status = status;
+}
+
+bool StratoMqttHandler::isInhibited()
+{
+    return (m_status == Status::Inhibited);
+}
+
+void StratoMqttHandler::setInhibited(bool inhibited)
+{
+    if (inhibited)
+    {
+        if (m_status == Status::Connected)
+        {
+            emit connection_status(Status::Inhibited);
+        }
+    }
+    else
+    {
+        emit connection_status(Status::Invalid);
+    }
+}
+
+StratoMqttHandler::StratoMqttHandler(const QString &station_id, const QString &host, unsigned port, const QString&, const int verbosity)
+    : QObject(nullptr), m_station_id{station_id.toStdString()}, m_host{host.toStdString()}, m_port{port}, m_verbose{verbosity}
+{
+    qRegisterMetaType<Status>("Status");
+
+    if (!connect(this, &StratoMqttHandler::connection_status, this, &StratoMqttHandler::set_status))
+    {
+        qDebug() << "failed connecting StratoMqttHandler::set_status";
+    }
+    if (!connect(this, &StratoMqttHandler::mqttConnect, this, &StratoMqttHandler::onMqttConnect))
+    {
+        qDebug() << "failed connecting StratoMqttHandler::mqttConnect";
+    }
+    if (!connect(this, &StratoMqttHandler::mqttDisconnect, this, &StratoMqttHandler::onMqttDisconnect))
+    {
+        qDebug() << "failed connecting StratoMqttHandler::mqttDisconnect";
+    }
+}
+
+StratoMqttHandler::~StratoMqttHandler()
+{
+    mqttDisconnect();
+    cleanup();
+}
+
+void StratoMqttHandler::start(const QString &username, const QString &password)
+{
+    m_username = username.toStdString();
+    m_password = password.toStdString();
+
+    CryptoPP::SHA1 sha1;
+    std::string source = username.toStdString() + m_station_id; // This will be randomly generated somehow
+    m_client_id = "";
+    CryptoPP::StringSource{source, true, new CryptoPP::HashFilter(sha1, new CryptoPP::HexEncoder(new CryptoPP::StringSink(m_client_id)))};
+
+    initialise(m_client_id);
+
+    emit mqttConnect();
+}
+
+void StratoMqttHandler::onMqttConnect()
+{
+    if (connected())
+    {
+        return;
+    }
+    qDebug() << "Trying to connect to MQTT.";
+
+    if (m_mqtt == nullptr)
+    {
+        qDebug() << "MQTT not initialized on connect called. Initializing...";
+        initialise(m_client_id);
+    }
+
+    emit connection_status(Status::Connecting);
+
+    auto result{mosquitto_username_pw_set(m_mqtt, m_username.c_str(), m_password.c_str())};
+    if (result != MOSQ_ERR_SUCCESS)
+    {
+        qWarning() << "Error setting username and password:" + QString{strerror(result)};
+        return;
+    }
+    result = mosquitto_connect_async(m_mqtt, Config::MQTT::host, Config::MQTT::port, Config::MQTT::keepalive_interval.count());
+    if (result != MOSQ_ERR_SUCCESS)
+    {
+        qDebug() << "Error on called mosquitto_connect_async";
+        return;
+    }
+}
+
+void StratoMqttHandler::onMqttDisconnect()
+{
+    if (m_mqtt != nullptr)
+    {
+        if (m_status == Status::Connected)
+        {
+            emit connection_status(Status::Disconnecting);
+            auto result{mosquitto_disconnect(m_mqtt)};
+            if (result == MOSQ_ERR_SUCCESS)
+            {
+                connection_status(Status::Disconnected);
+            }
+            else
+            {
+                qDebug() << "Could not disconnect from Mqtt: " + QString{strerror(result)};
+                connection_status(Status::Invalid);
+            }
+        }
+    }
+}
+
+auto StratoMqttHandler::connected() -> bool
+{
+    return (m_mqtt != nullptr) && (m_status == Status::Connected);
+}
+
+void StratoMqttHandler::initialise(const std::string &client_id)
+{
+    if (m_mqtt != nullptr)
+    {
+        cleanup();
+    }
+
+    mosquitto_lib_init();
+
+    m_mqtt = mosquitto_new(client_id.c_str(), true, this);
+
+    // exponential backoff set to true. Example retry periods: 2, 4, 8, 16, 32 ...
+    mosquitto_reconnect_delay_set(m_mqtt, first_retry_period, max_retry_period, true);
+
+    mosquitto_connect_callback_set(m_mqtt, wrapper_callback_connected);
+    mosquitto_disconnect_callback_set(m_mqtt, wrapper_callback_disconnected);
+    mosquitto_message_callback_set(m_mqtt, wrapper_callback_message);
+
+    mosquitto_loop_start(m_mqtt);
+}
+
+void StratoMqttHandler::cleanup()
+{
+    if (connected())
+    {
+        mqttDisconnect();
+    }
+    if (m_mqtt != nullptr)
+    {
+        mosquitto_loop_stop(m_mqtt, true);
+
+        mosquitto_destroy(m_mqtt);
+        m_mqtt = nullptr;
+        mosquitto_lib_cleanup();
+    }
+}
+
+void StratoMqttHandler::subscribe(const QString &topic)
+{
+    if (!connected())
+    {
+        return;
+    }
+
+    m_topics.emplace_back(topic.toStdString());
+
+    auto result{mosquitto_subscribe(m_mqtt, nullptr, topic.toStdString().c_str(), 1)};
+
+    if (result != MOSQ_ERR_SUCCESS)
+    {
+        switch (result)
+        {
+        case MOSQ_ERR_INVAL:
+            qWarning() << "Could not subscribe to topic '" + topic + "': invalid parameters";
+            break;
+        case MOSQ_ERR_NOMEM:
+            qWarning() << "Could not subscribe to topic '" + topic + "': memory exceeded";
+            break;
+        case MOSQ_ERR_NO_CONN:
+            qWarning() << "Could not subscribe to topic '" + topic + "': Not connected";
+            break;
+        case MOSQ_ERR_MALFORMED_UTF8:
+            qWarning() << "Could not subscribe to topic '" + topic + "': malformed utf8";
+            break;
+        default:
+            qWarning() << "Could not subscribe to topic '" + topic + "': other reason";
+            break;
+        }
+        return;
+    }
+    qInfo() << "Subscribed to topic '" + topic + "'.";
+}
+
+void StratoMqttHandler::unsubscribe(const QString &topic)
+{
+    if (!connected())
+    {
+        return;
+    }
+
+    auto pos{std::find(std::begin(m_topics), std::end(m_topics), topic.toStdString())};
+    if (pos != std::end(m_topics))
+    {
+        m_topics.erase(pos);
+    }
+
+    auto result{mosquitto_unsubscribe(m_mqtt, nullptr, topic.toStdString().c_str())};
+
+    if (result != MOSQ_ERR_SUCCESS)
+    {
+        switch (result)
+        {
+        case MOSQ_ERR_INVAL:
+            qWarning() << "Could not unsubscribe from topic '" + topic + "': invalid parameters";
+            break;
+        case MOSQ_ERR_NOMEM:
+            qWarning() << "Could not unsubscribe from topic '" + topic + "': memory exceeded";
+            break;
+        case MOSQ_ERR_NO_CONN:
+            qWarning() << "Could not unsubscribe from topic '" + topic + "': Not connected";
+            break;
+        case MOSQ_ERR_MALFORMED_UTF8:
+            qWarning() << "Could not unsubscribe from topic '" + topic + "': malformed utf8";
+            break;
+        default:
+            qWarning() << "Could not unsubscribe from topic '" + topic + "': other reason";
+            break;
+        }
+        return;
+    }
+    qInfo() << "Unsubscribed from topic '" + topic + "'.";
+}
+
+void StratoMqttHandler::publish(const QString &topic, const QString &content)
+{
+    if (!connected())
+    {
+        return;
+    }
+    // std::string usertopic{topic.toStdString()};
+    // usertopic += m_username + "/" + m_station_id;
+    if (!publish(topic, content.toStdString()))
+    {
+        m_publish_error_count++;
+        if (m_publish_error_count < s_max_publish_errors)
+        {
+            qWarning() << "Couldn't publish message for topic" << topic;
+        }
+        else if (m_publish_error_count == s_max_publish_errors)
+        {
+            qWarning() << "Couldn't publish message for topic" << topic << "(message repeated" << s_max_publish_errors << "times)";
+        }
+        return;
+    }
+    m_publish_error_count = 0;
+}
+
+auto StratoMqttHandler::publish_(const std::string &topic, const std::string &content) -> bool
+{
+    if (!connected())
+    {
+        return false;
+    }
+    auto result{mosquitto_publish(m_mqtt, nullptr, topic.c_str(), static_cast<int>(content.size()), reinterpret_cast<const void *>(content.c_str()), 1, false)};
+
+    if (result == MOSQ_ERR_SUCCESS)
+    {
+        return true;
+    }
+    qWarning() << "Couldn't publish mqtt message: " + QString{strerror(result)};
+    return false;
+}
+
+void StratoMqttHandler::requestConnectionStatus()
+{
+    qDebug() << "connection status = " << QString::number(static_cast<int>(m_status));
+    emit connection_status(m_status);
+}
